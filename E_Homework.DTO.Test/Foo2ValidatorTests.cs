7ea7f90d8a60aa7a06ee205cda5aa008beca71c4
Ed Johnson <ad5mq@outlook.com>
2023-05-26 13:58:25 -0600

    completed Foo2 Vaildator tests

--- E_Homework.DTO.Test/Foo2ValidatorTests.cs
+++ E_Homework.DTO.Test/Foo2ValidatorTests.cs
@@ -37,12 +37,123 @@ namespace E_Homework.DTO.Test
 
         #region Tests
         [Fact]
-        void HappyPathFoo1Succeeds()
+        void HappyPathFoo2Succeeds()
         {
             Foo2 f2 = GetValidFoo2();
             var validator = new Foo2Validator();
             var result = validator.Validate(f2);
             result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void SerDesSuccess()
+        {
+            Foo2 retf = GetValidFoo2();
+            var js = JsonSerializer.Serialize<Foo2>(retf, options);
+            var fd = JsonSerializer.Deserialize<Foo2>(js, options);
+            retf.ShouldDeepEqual(fd);
+
+        }
+
+        [Fact]
+        void FailsWithEmptyCompany()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Company = string.Empty;
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        void FailsWithBadCompanyId()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.CompanyId = 0;
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+        void FailsWithNoDevices()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Devices = new List<Device>();
+            //break valid data
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        void FailsWithBadDeviceId()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Devices[0].DeviceID = 0;
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        void FailsWithBadDeviceModel()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Devices[0].Name = string.Empty;
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+        void FailsWithBadDeviceDate()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Devices[0].StartDateTime = DateTimeOffset.MinValue;
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        void FailsWithNoSensorData()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Devices[0].SensorData = new List<F2Sensor>();
+            //break valid data
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+
+        [Fact]
+        void FailsWithBadSensorDataDate()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Devices[0].SensorData[0].DateTime = DateTimeOffset.MinValue;
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        void FailsWithBadSensorDataValue()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Devices[0].SensorData[0].Value = float.NaN;
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        void FailsWithBadSensorDataType()
+        {
+            Foo2 f2 = GetValidFoo2();
+            f2.Devices[0].SensorData[0].SensorType = "Barometer";
+            var validator = new Foo2Validator();
+            var result = validator.Validate(f2);
+            result.IsValid.Should().BeFalse();
         }
         #endregion Tests
 
